4e6b056d2dfceaf7914a88e30d5a71981c28f704
Jilsjoseph3030 <jilsjoseph3030@gmail.com>
2026-07-28 00:16:43 +0530

    fixed deploying issue

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { db, auth } from "../firebase";
+import { db } from "../firebase";
 import { useNavigate } from "react-router-dom";
 import { collection, onSnapshot, query, orderBy, deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { useToast } from "./Toast";
